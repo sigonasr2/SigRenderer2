c9366ba54151b865dc881aecc3c3e69520136610
Joshua Sigona <sigonasr2@gmail.com>
2021-11-17 16:26:14 +0900

    Fix plant texture coordinates

--- src/sig/SigRenderer.java
+++ src/sig/SigRenderer.java
@@ -401,7 +401,9 @@ public class SigRenderer implements WindowFocusListener,KeyListener,MouseListene
         Random r = new Random(438107);
         for (int x=0;x<64;x++) {
             for (int z=0;z<64;z++) {
-                addBlock(new Vector(x,0,z),Cube.class,BlockType.GRASS,FacingDirection.SOUTH);
+                addBlock(new Vector(x,0,z),Cube.class,BlockType.SOIL_WET,FacingDirection.SOUTH);
+                addBlock(new Vector(x,1,z),Plant.class,BlockType.valueOf("WHEAT_"+(r.nextInt(7))),FacingDirection.SOUTH);
+
                 //addBlock(new Vector(x,1,z),Staircase.class,BlockType.JUNGLE_PLANK,FacingDirection.SOUTH);
                 //addBlock(new Vector(x,2,z),Staircase.class,BlockType.SPRUCE_PLANK,FacingDirection.SOUTH);
                 /*for (int y=1;y<r.nextInt(5);y++) {
@@ -428,10 +430,6 @@ public class SigRenderer implements WindowFocusListener,KeyListener,MouseListene
                 }*/
             }
         }
-        addBlock(new Vector(31,1,31),Plant.class,BlockType.SUGARCANE,FacingDirection.NORTH);
-        addBlock(new Vector(31,2,31),Plant.class,BlockType.SUGARCANE,FacingDirection.NORTH);
-        addBlock(new Vector(31,3,31),Plant.class,BlockType.SUGARCANE,FacingDirection.NORTH);
-        addBlock(new Vector(31,4,31),Plant.class,BlockType.SUGARCANE,FacingDirection.NORTH);
         /*addBlock(new Vector(31,2,32),Staircase.class,BlockType.PLANKS,FacingDirection.EAST);
         addBlock(new Vector(31,3,33),Staircase.class,BlockType.PLANKS,FacingDirection.WEST);
         addBlock(new Vector(31,4,34),Staircase.class,BlockType.PLANKS,FacingDirection.SOUTH);
